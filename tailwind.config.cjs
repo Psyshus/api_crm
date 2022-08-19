f7b51403c912aa92fbe89919f9ddebf5f833e2ef
psyshus <erick.vasquez.lopez00@gmail.com>
2022-08-19 12:21:19 -0500

    Proyecto terminado, arreglo en variables de entorno

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.js', './src/**/*.jsx'],
+  content: ['index.html', './src/**/*.js', './src/**/*.jsx'],
   theme: {
     extend: {},
   },
